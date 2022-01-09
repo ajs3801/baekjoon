7762abea75600b4a339f620ec0f265f4e3586a3d
ANJunSeong <tt14an@gmail.com>
2022-01-09 11:54:33 +0900

    Update 2577.java
    자바는 0으로 초기화할 필요 없음

--- 2577.java
+++ 2577.java
@@ -5,7 +5,8 @@ public class Main {
     Scanner sc = new Scanner(System.in);
     
     int[] numArray = new int[10];
-    for (Integer i:numArray) {numArray[i] = 0;}
+    //자바는 0으로 초기화할 필요 없음
+    for (Integer i:numArray) {i=0;}
 
     int num1 = sc.nextInt();
     int num2 = sc.nextInt();
